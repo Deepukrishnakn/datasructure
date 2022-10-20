a971a4c890e08bda41f5c7f789fbc5ffe7755395
deepukrishna kn <deepukrishna25@gmsil.com>
2022-10-20 22:25:28 +0530

    fsf

--- selection_sort.py
+++ selection_sort.py
@@ -1,13 +1,12 @@
-# ith using min and max methods
 
-# list1=[2,3,5,5,3,33,70]
-# print(list1)
-# for i in range(len(list1)-1):
-#     min_val = max(list1[i:])
-#     min_index = list1.index(min_val,i)
-#     if list1[i] != list1[min_index]:
-#         list1[i],list1[min_index] = list1[min_index],list1[i]
-#         print(list1)
+list1=[2,3,5,5,3,33,70]
+print(list1)
+for i in range(len(list1)-1):
+    min_val = max(list1[i:])
+    min_index = list1.index(min_val,i)
+    if list1[i] != list1[min_index]:
+        list1[i],list1[min_index] = list1[min_index],list1[i]
+        print(list1)
 
 #--------------------------------------------------------------------------------
 
