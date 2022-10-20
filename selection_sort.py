1208bde87f58b6f2951d7735c648ac2baae06433
deepukrishna kn <deepukrishna25@gmsil.com>
2022-10-20 22:30:58 +0530

    fsf

--- selection_sort.py
+++ selection_sort.py
@@ -1,16 +1,17 @@
 
-list1=[2,3,5,5,3,33,70]
-print(list1)
-for i in range(len(list1)-1):
-    min_val = max(list1[i:])
-    min_index = list1.index(min_val,i)
-    if list1[i] != list1[min_index]:
-        list1[i],list1[min_index] = list1[min_index],list1[i]
-        print(list1)
+# list1=[2,3,5,5,3,33,70]
+# print(list1)
+# for i in range(len(list1)-1):
+#     min_val = max(list1[i:])
+#     min_index = list1.index(min_val,i)
+#     if list1[i] != list1[min_index]:
+#         list1[i],list1[min_index] = list1[min_index],list1[i]
+#         print(list1)
 
 #--------------------------------------------------------------------------------
 
-list1=[22,35,50,5,3,33,70]
+num = int(input('enter a limit'))
+list1=[int(input('enter elements')) for i in range(num)]
 print(list1)
 for i in range(len(list1)-1):
     min_index = i
