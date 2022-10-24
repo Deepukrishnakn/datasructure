9cbceda5fb6ca77025947797fbccc2c0d11f78b1
deepukrishna kn <deepukrishna25@gmsil.com>
2022-10-24 15:24:46 +0530

    delete

--- BST.py
+++ BST.py
@@ -24,6 +24,7 @@ class BST:
             else:
                 self.rchild=BST(data)
 
+    #BST_Search----------------------------------------------
     def search(self,data):
         if self.key == data:
             print('key is found')
@@ -38,7 +39,8 @@ class BST:
                 self.rchild.search(data)
             else:
                 print('key is not found')
-    
+
+    #preorder----------------------------------------------------
     def preorder(self):
         print(self.key)
         if self.lchild:
@@ -46,13 +48,59 @@ class BST:
         if self.rchild:
             self.rchild.preorder()
 
+    #Inorder------------------------------------------------------
+    def inorder(self):
+        if self.lchild:
+            self.lchild.preorder()
+        print(self.key)
+        if self.rchild:
+            self.rchild.preorder()
 
+    #postorrder---------------------------------------------------
+    def postorder(self):
+        if self.lchild:
+            self.lchild.postorder()
+        if self.rchild:
+            self.rchild.postorder()
+        print(self.key)
+    
+    #delete----------------------------------------------------------
+    def delete(self,data):
+        if self.key is None:
+            print('Tree is empty')
+            return
+        if data < self.key:
+            if self.lchild:
+                self.lchild = self.lchild.delete(data)
+            else:
+                print('node is not present')
+        elif data > self.key:
+            if self.rchild:
+                self.rchild = self.rchild.delete(data)
+            else:
+                print('node is not present')
+        else:
+            if self.lchild is None:
+                temp = self.rchild
+                self = None
+                return temp
+            if self.rchild is None:
+                temp = self.lchild
+                return temp
+            node = self.rchild
+            while node.lchild:
+                node=node.lchild
+            self.key=node.key
+            self.rchild=self.rchild.delete(node.key)
+        return self
+            
 
 
 root = BST(10)
 list1=[3,5,44,21,7,5,90]
 for i in list1:
     root.insert(i)
-root.preorder()
+root.delete(10)
+root.postorder()
 
 
